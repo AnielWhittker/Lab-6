823f6473ea355771e8ad1df6429bf187baade89b
AnielWhittker <whittkermelian.a@ufl.edu>
2022-10-24 18:53:24 -0400

    modified program

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def addition(x, y):
     return x + y
 
+
 x = float(input())
 y = float(input())
 
 print(addition(x, y))
+
+
+def subtract(a, b):
+    return a - b
+
+
+a = float(input())
+b = float(input())
+
+print(subtract(a, b))
